b349944b946f0c00df58f755f1f770d4a64bf7ba
demon000 <demonsingur@gmail.com>
2019-06-27 07:47:38 -0400

    Patch qti-telephony-common to remove MIUI dependencies.
    diff --git a/smaddli/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.smali b/smali/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.smali
    --- a/smali/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.smali
    +++ b/smali/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.smali
    @@ -17,17 +17,13 @@
         .locals 1
    
         .line 12
    -    sget-boolean v0, Lmiui/os/Build;->IS_GLOBAL_BUILD:Z
    +    const v0, 1
    
         return v0
     .end method
    
     .method public static setSARLimit(I)V
         .locals 0
         .param p0, "set"    # I
    -
    -    .line 8
    -    invoke-static {p0}, Landroid/net/wifi/MiuiWifiManager;->setSARLimit(I)V
    
         .line 9
         return-void
    
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- sources/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.java
+++ sources/com/qualcomm/qti/internal/telephony/WifiManagerCompatible.java
@@ -1,14 +1,10 @@
 package com.qualcomm.qti.internal.telephony;
 
-import android.net.wifi.MiuiWifiManager;
-import miui.os.Build;
-
 public class WifiManagerCompatible {
     public static void setSARLimit(int set) {
-        MiuiWifiManager.setSARLimit(set);
     }
 
     public static boolean isNeeded() {
-        return Build.IS_GLOBAL_BUILD;
+        return true;
     }
 }
